a2a5a27b5c5a8e046b66e437e618a63a6412540e
Mhart400 <mhart400@gmail.com>
2021-06-20 10:08:52 -0400

    ok

---
+++ src/components/VotingRecord.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class VotingRecord extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+
+
+export default VotingRecord;
